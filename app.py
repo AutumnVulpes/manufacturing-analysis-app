90001b65c28dfe4d5921ee3d7344f6601812603f
AutumnVulpes <johnhongren@gmail.com>
2025-07-02 00:44:05 +0800

    Add import kaleido

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+import kaleido
 
 
 def initialise_preloaded_data():
